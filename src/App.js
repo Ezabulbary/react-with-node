28d0638e5fb2c1a54dbee5885cd9aec50cdb68aa
Ezabulbary <ezabul.bary@gmail.com>
2022-05-08 22:46:17 +0600

    data send api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+
+  const handleToSubmit = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    console.log(name, email)
+
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+  }
+
+
   return (
     <div className="App">
       <h1>My First react with node</h1>
+      <p>Users: {users.length}</p>
+      <form onSubmit={handleToSubmit}>
+        <input type="text" name="name" placeholder='Name' required />
+        <input type="text" name="email" placeholder='Email' required />
+        <input type="submit" value="Add User" />
+      </form>
+      <ul>
+        {
+          users.map(user => <li key={user.id}>Id: {user.id}, Name: {user.name} Job:{user.job}</li>)
+        }
+      </ul>
     </div>
   );
 }
